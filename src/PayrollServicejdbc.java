fac077b319ce1084d03fe23f751ab841190e2650
Sreedhar2484 <nagallisreedhar1918@gmail.com>
2023-02-28 18:01:32 +0530

    added : program to retrive data by getting salary by gender

--- src/PayrollServicejdbc.java
+++ src/PayrollServicejdbc.java
@@ -1,4 +1,4 @@
-package com.bridgelabz.databaseconnection.databaseConnection;
+package com.bridgelabz.databaseconnection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,38 +7,24 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class PayrollServiceJdbc {
+
     public static void main(String args[]) {
-        try {
-            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/payrollservice", "root",
-                    "root");
-            Statement statement = connect.createStatement();
-            statement.execute("update employee_payroll set basic_pay=3000000 where name = 'terissa';");
+		try {
+			Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/payrollservice", "root",
+					"root");
+			Statement statement = connect.createStatement();
+			statement.execute("SELECT SUM(salary) FROM employee_payroll WHERE gender = 'F' GROUP BY gender;");
+			statement.execute("SELECT AVG(salary) FROM employee_payroll WHERE gender = 'F' GROUP BY gender;");
+			statement.execute("SELECT MIN(salary) FROM employee_payroll WHERE gender = 'F' GROUP BY gender;");
+			statement.execute("SELECT MAX(salary) FROM employee_payroll WHERE gender = 'F' GROUP BY gender;");
+			statement.execute("SELECT COUNT(salary) FROM employee_payroll WHERE gender = 'F' GROUP BY gender;");
 
-            if (statement != null) {
-                System.out.println("connection is successful:");
-            }
-            ResultSet output = statement.executeQuery(
-                    "select * from employee_payroll WHERE start_date BETWEEN CAST('2020-04-15' AS DATE) AND DATE(NOW());");
+			if (statement != null) {
+				System.out.println("connection is successful:");
+			}
 
-            while (output.next()) {
-                System.out.print("id: " + output.getInt(1) + "\t");
-                System.out.println("name: " + output.getString(2));
-                System.out.println("salary: " + output.getInt(3));
-                System.out.println("date: " + output.getString(4));
-                System.out.println("gender: " + output.getString(5));
-                System.out.println("phone: " + output.getLong(6));
-                System.out.println("address: " + output.getString(7));
-                System.out.println("department: " + output.getString(8));
-                System.out.println("basicPay: " + output.getInt(9));
-                System.out.println("deductions: " + output.getInt(10));
-                System.out.println("taxablePay: " + output.getInt(11));
-                System.out.println("incometax: " + output.getInt(12));
-                System.out.println("netpay: " + output.getInt(13));
-                System.out.println();
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
